c7f9b9355bca01b63bd6cd0f84f378b22ee9c251
Veronika Dunaeva <dunaevaveronika13@gmail.com>
2025-06-09 01:45:21 +0300

    Added main logic to memoization fn

--- memoize.js
+++ memoize.js
@@ -4,5 +4,14 @@ function memoize(fn, ttl = 10 * 60 * 1000) {
   return (...args) => {
     const key = JSON.stringify(args);
     const now = Date.now();
+
+    if (cache.has(key)) {
+      const { time, result } = cache.get(key);
+      if (now - time < ttl) return result;
+    }
+
+    const result = fn(...args);
+    cache.set(key, { time: now, result });
+    return result;
   };
 }
