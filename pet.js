2b559f6bcb08a9d5a46e55589b9f0f6f854e85cc
Veronika Dunaeva <dunaevaveronika13@gmail.com>
2025-06-09 18:22:33 +0300

    added pet status update

--- pet.js
+++ pet.js
@@ -7,14 +7,28 @@ let energy = 100;
 
 const calcEnergy = memoize((enrg) => Math.max(0, enrg - 25));
 
+function updateStatus() {
+  let sleepStatus =
+    energy <= 25 ? "Pet wants to sleep" : "Pet is full of energy";
+  let hungerStatus = hunger >= 50 ? "Pet is hungry" : "Pet doesn`t want to eat";
+
+  const statusMessage = `Pet's hunger now is ${hunger}. ${hungerStatus}. The level of energy: ${energy}. ${sleepStatus}.\n`;
+
+  fs.writeFile("pet_status.txt", statusMessage, (err) => {
+    if (err) console.error("Error writing status:", err);
+  });
+}
+
 function feed() {
   hunger = Math.max(0, hunger - 15);
+  updateStatus();
   petEvents.emit("Feed");
   log("HUNGER", `hunger=${hunger}`);
 }
 
 function sleep() {
   energy = Math.max(100, energy + 30);
+  updateStatus();
   petEvents.emit("Sleep");
   log("SLEEP", `energy=${energy}`);
 }
@@ -22,6 +36,7 @@ function sleep() {
 function play() {
   energy = calcEnergy(energy);
   hunger += 5;
+  updateStatus();
   petEvents.emit("Play");
   log("PLAY", `energy=${energy}`, `hunger=${hunger}`);
 }
